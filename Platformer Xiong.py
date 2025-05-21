750440116f9d6ea296ff0399ae35fdd9792fb407
JXiong710881 <JXiong710881@muhsdstudents.org>
2025-05-21 11:40:12 -0700

    Add files via upload

--- Platformer Xiong.py
+++ Platformer Xiong.py
@@ -15,12 +15,14 @@ pygame.display.set_caption('Platformer')
 #define game variable
 tile_size = 50
 game_over = 0
+main_menu = True
 
 #load images
 sun_img = pygame.image.load('img/sun.png')
 bg_img = pygame.image.load('img/sky.png')
 restart_img = pygame.image.load('img/restart_btn.png')
-
+start_img = pygame.image.load('img/start_btn.png')
+exit_img = pygame.image.load('img/exit_btn.png')
 
 
 class Button():
@@ -293,6 +295,9 @@ world = World(world_data)
 
 #create buttons
 restart_button = Button(screen_width // 2 - 50, screen_height // 2 + 100, restart_img)
+start_button = Button(screen_width // 2 - 350, screen_height // 2, start_img)
+exit_button = Button(screen_width // 2 + 150, screen_height // 2, exit_img)
+
 
 run = True
 while run:
@@ -302,21 +307,29 @@ while run:
     screen.blit(bg_img, (0, 0))
     screen.blit(sun_img, (100, 100))
 
-    world.draw()
 
-    if game_over == 0:
-        blob_group.update()
+    if main_menu == True:
+        if exit_button.draw():
+            run = False
+        if start_button.draw():
+            main_menu = False
+    else:
+        world.draw()
         
-    blob_group.draw(screen)
-    lava_group.draw(screen)
 
-    game_over = player.update(game_over)
+        if game_over == 0:
+            blob_group.update()
+            
+        blob_group.draw(screen)
+        lava_group.draw(screen)
 
-    #if player has died
-    if game_over == -1:
-       if restart_button.draw():
-           player.reset(100, screen_height - 130)
-           game_over = 0
+        game_over = player.update(game_over)
+
+        #if player has died
+        if game_over == -1:
+           if restart_button.draw():
+               player.reset(100, screen_height - 130)
+               game_over = 0
         
     
     for event in pygame.event.get():
